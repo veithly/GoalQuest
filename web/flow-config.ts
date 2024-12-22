e0af132e9c53002158db2c8b1eb09f7b4cdd2453
Bean <bean1730137@gmail.com>
2024-12-22 12:41:43 +0800

    feat: 连接测试网

--- web/flow-config.ts
+++ web/flow-config.ts
@@ -1,7 +1,8 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "accessNode.api": "https://rest-mainnet.onflow.org",
-  "flow.network": "mainnet",
+  // "accessNode.api": "https://rest-mainnet.onflow.org",
+  "flow.network": "testnet",
+  "app.detail.title": "GoalQuest", // DApp 名称
   "discovery.wallet": "https://fcl-discovery.onflow.org/authn",
 });
